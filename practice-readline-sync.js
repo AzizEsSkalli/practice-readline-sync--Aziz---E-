8d44118d973683b7739c417a13730d4bdd66400b
Aziz Es Skalli <aziz.esskalli7@gmail.com>
2025-03-17 07:11:17 +0100

    practice-readline-sync-CommandLineQuizApp

--- practice-readline-sync.js
+++ practice-readline-sync.js
@@ -1,1 +1,12 @@
+const input = require('readline-sync');
+
+let name = input.question("What's your name? ");
+console.log("Hello, " + name + "!");
+let dataType = input.question("What are the data types? ");
+let conversionType = Number(input.question("How many types of data conversion in JavaScript? "));
+let booleanValue = input.question("Which values does the boolean accept? ");
+let stringAddition = input.question("What does it mean concatenation? ");
+let stringToNumber = input.question("Which function we use to convert string to number? ")
+console.log(dataType, "  " + conversionType, "  " + booleanValue, "  " + stringAddition, "  " + stringToNumber);
+
 
